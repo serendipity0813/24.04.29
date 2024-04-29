7533f183380fe6356fa3e2e4ab41edb67798ce22
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-04-29 18:42:50 +0900

    [Add] SlotLogic

--- Assets/Scripts/Controller/SlotController.cs
+++ Assets/Scripts/Controller/SlotController.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotController : MonoBehaviour
 {
     private TextMeshProUGUI _numberText;
+    private Image[] _image;
     int _line = 0;
     int _column = 0;
+    int _number = 0;
     string _objectName = "";
 
     private void Awake()
     {
         _numberText = GetComponentInChildren<TextMeshProUGUI>();
+        _image = GetComponentsInChildren<Image>();
         _objectName = this.gameObject.name;
         _line = Int32.Parse(_objectName) / 4;
         _column = Int32.Parse(_objectName) % 4;
@@ -25,12 +29,64 @@ public class SlotController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ChangeNumber();
+            _number = GameManager.Instance.Numbers[_line, _column];               
+             ChangeNumber();
+             ChangeColor();
+                     
+        }
+    }
+
+    private void ChangeColor()
+    {
+        switch(_number)
+        {
+            case 1:
+                _image[1].color = new Color(0, 0, 0, 0);
+                    break;
+            case 2:
+                _image[1].color = new Color(210, 201, 192, 255);
+                break;
+            case 4:
+                _image[1].color = new Color(229, 217, 194, 255);
+                break;
+            case 8:
+                _image[1].color = new Color(228, 165, 113, 225);
+                break;
+            case 16:
+                _image[1].color = new Color(238, 143, 96, 225);
+                break;
+            case 32:
+                _image[1].color = new Color(234, 119, 91, 225);
+                break;
+            case 64:
+                _image[1].color = new Color(238, 90, 59, 225);
+                break;
+            case 128:
+                _image[1].color = new Color(227, 198, 108, 225);
+                break;
+            case 256:
+                _image[1].color = new Color(232, 99, 96, 225);
+                break;
+            case 512:
+                _image[1].color = new Color(229, 94, 77, 225);
+                break;
+            case 1024:
+                _image[1].color = new Color(235, 193, 66, 255);
+                break;
+            case 2048:
+                _image[1].color = new Color(230, 191, 61, 225);
+                break;
+            default:
+                _image[1].color = new Color(255, 255, 255, 255);
+                break;
         }
     }
 
     private void ChangeNumber()
     {
-        _numberText.text = GameManager.Instance.Numbers[_line, _column].ToString();
+        if (_number == 1)
+            _numberText.text = "";
+        else
+            _numberText.text = _number.ToString();
     }
 }
--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,14 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public int[,] Numbers = new int[4, 4];
-    public event EventHandler Click;
+    private int[] _newNumber = new int[10];
+    private int _maxNumber = 1;
+    private int _maxCount = 0;
 
     private void Awake()
     {
@@ -25,18 +27,232 @@ public class GameManager : MonoBehaviour
                 Numbers[i, j] = 1;
             }
         }
+
+        for(int i=0; i<10; i++)
+        {
+            if (i == 0)
+                _newNumber[i] = 2;
+            else
+                _newNumber[i] = _newNumber[i-1] * 2;
+        }
     }
 
     private void Update()
     {
+
         if(Input.GetMouseButtonDown(0))
         {
-            OnClick();
+            int count = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (Numbers[i, j] != 1)
+                        count++;
+                }
+            }
+
+            if (count == 16)
+                GameOver();
+
+            else
+            {
+                float x = Input.mousePosition.x;
+                float y = Input.mousePosition.y;
+
+                if (System.Math.Abs(x) >= System.Math.Abs(y))
+                {
+                    if (x > 0)
+                        MoveRight();
+                    else
+                        MoveLeft();
+                }
+                else
+                {
+                    if (y > 0)
+                        MoveUp();
+                    else
+                        MoveDown();
+                }
+            }
+            
         }
     }
 
-    private void OnClick()
+    private void GameOver()
     {
-        Debug.Log("click");
+        _maxNumber = 1;
+        _maxCount = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Numbers[i, j] = 1;
+
+
+            }
+        }
+    }
+
+    private void MoveDown()
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if ((Numbers[i, j]) != 1)
+                {
+                    if ((Numbers[i+1, j]) == 1)
+                    {
+                        Numbers[i+1, j] = Numbers[i, j];
+                        Numbers[i, j] = 1;
+                    }
+                    else if (Numbers[i+1, j] == Numbers[i, j])
+                    {
+                        Numbers[i+1, j] = Numbers[i+1, j] + Numbers[i, j];
+                        Numbers[i, j] = 1;
+                        if(Numbers[i + 1, j] > _maxNumber)
+                        {
+                            _maxCount++;
+                            _maxNumber = Numbers[i + 1, j];
+                        }
+
+                    }
+                }
+
+            }
+        }
+        MakeRandomSlot();
+    }
+
+    private void MoveUp()
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int i = 3; i > 0; i--)
+            {
+                if ((Numbers[i, j]) != 1)
+                {
+                    if ((Numbers[i - 1, j]) == 1)
+                    {
+                        Numbers[i - 1, j] = Numbers[i, j];
+                        Numbers[i, j] = 1;
+                    }
+                    else if (Numbers[i - 1, j] == Numbers[i, j])
+                    {
+                        Numbers[i - 1, j] = Numbers[i - 1, j] + Numbers[i, j];
+                        Numbers[i, j] = 1;
+                        if (Numbers[i - 1, j] > _maxNumber)
+                        {
+                            _maxCount++;
+                            _maxNumber = Numbers[i - 1, j];
+                        }
+                    }
+                }
+
+            }
+        }
+        MakeRandomSlot();
+    }
+
+    private void MoveLeft()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 3; j > 0; j--)
+            {
+                if ((Numbers[i, j]) != 1)
+                {
+                    if ((Numbers[i, j - 1]) == 1)
+                    {
+                        Numbers[i, j - 1] = Numbers[i, j];
+                        Numbers[i, j] = 1;
+                    }
+                    else if (Numbers[i, j - 1] == Numbers[i, j])
+                    {
+                        Numbers[i, j - 1] = Numbers[i, j - 1] + Numbers[i, j];
+                        Numbers[i, j] = 1;
+                        if (Numbers[i, j-1] > _maxNumber)
+                        {
+                            _maxCount++;
+                            _maxNumber = Numbers[i, j-1];
+                        }
+                    }
+                }
+
+            }
+        }
+        MakeRandomSlot();
+    }
+
+    private void MoveRight()
+    {
+        for(int i=0; i<4; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                if ((Numbers[i, j]) != 1)
+                {
+                    if ((Numbers[i, j+1]) == 1)
+                    {
+                        Numbers[i, j + 1] = Numbers[i, j];
+                        Numbers[i, j] = 1;
+                    }
+                    else if (Numbers[i, j+1] == Numbers[i,j])
+                    {
+                        Numbers[i, j + 1] = Numbers[i, j + 1] + Numbers[i, j];
+                        Numbers[i, j] = 1;
+                        if (Numbers[i, j + 1] > _maxNumber)
+                        {
+                            _maxCount++;
+                            _maxNumber = Numbers[i, j + 1];
+                        }
+                    }
+                }
+                  
+            }
+        }
+
+        MakeRandomSlot();
+    }
+
+    private void MakeRandomSlot()
+    {
+        bool flag = false;
+        int count = 0;
+        int newNum = 0;
+        int rnd = 0;
+        if (_maxCount < 4)
+            newNum = 2;
+        else if (_maxCount < 13)
+        {
+            rnd = Random.Range(0, _maxCount - 3);
+            newNum = _newNumber[rnd];
+        }
+        else
+            newNum = _newNumber[Random.Range(0, 10)];
+
+        for(int i=0; i<4; i++)
+        {
+            for(int j=0; j<4; j++)
+            {
+                if (Numbers[i, j] != 1)
+                    count++;
+            }
+        }
+
+
+        while (!flag)
+        {
+            int line = Random.Range(0, 4);
+            int column = Random.Range(0, 4);
+
+            if (Numbers[line, column] == 1)
+            {
+                Numbers[line, column] = newNum;
+                flag = true;
+            }
+                
+        }
     }
 }
